64beed4166166e58f91b5e47c010fdec2e0a1c8d
Adari Girish Kumar <adari.girishkumar@gmail.com>
2022-06-27 23:30:39 +0530

    Time: 2 ms (33.58%), Space: 42.6 MB (12.34%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String interpret(String command) {
+        
+        String ans = command.replace("()","o");
+        ans = ans.replace("(al)","al"); 
+        return ans;
+    }
+        
+}
