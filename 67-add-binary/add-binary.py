d89cefe0bf000cae99d892e34fbb0fd97962ed9f
Girish Kumar Adari <adari.girishkumar@gmail.com>
2024-06-17 20:22:10 -0400

    Time: 35 ms (72.11%) | Memory: 16.6 MB (61.59%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,15 +1,4 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a=int(a,2)
-        b=int(b,2)
-        ans=a+b
-
-        if(a==0 and b==0):
-            return "0"
-
-        mod= ""
-        while(ans >= 1):
-            mod += str(int(ans%2))
-            ans = ans//2
-        return mod[::-1]
+        return "{0:b}".format(int(a, 2) + int(b, 2))
         
